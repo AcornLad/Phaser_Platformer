1ffd38bcb3b863eb9978b1dda9d6d40e56d0d5bc
Conrad Cash <conradcash@gmail.com>
2018-01-06 20:23:54 -0500

    Added some objects and enabled ARCADE physics

--- view/game.js
+++ view/game.js
@@ -1,6 +1,7 @@
 //https://phaser.io/tutorials/making-your-first-phaser-game/part2
 
 var platforms;
+var player;
 
 var state = {
     preload: function(){
@@ -38,9 +39,25 @@ var state = {
         ledge = platforms.create(-150, 250, 'ground');
         
         ledge.body.immovable = true;
+        
+        //The player and its properties
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        
+        //Enable physics on player
+        game.physics.arcade.enable(player);
+        
+        //Player physics properties. Give him a li'l bounce
+        player.body.bounce.y = 0.2;
+        player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+        
+        //Our two animations, walking left and right. The 'true' is an answer to some loop? boolean
+        player.animations.add('left', [0, 1, 2, 3], 10, true);
+        player.animations.add('right', [5, 6, 7, 8], 10, true);
     },
     update: function(){
-        
+        //Collide the player and the stars with the platforms
+        var hitPlatform = game.physics.arcade.collide(player, platforms);
     }
 }
 
